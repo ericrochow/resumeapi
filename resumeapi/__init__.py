075717ccfc1c6f5bcd8c689c235f634a8438abc5
Eric Rochow <=>
2022-10-03 13:54:08 -0400

    get rid of path hacks and use absolute imports

--- resumeapi/__init__.py
+++ resumeapi/__init__.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
+"""Import modules."""
 
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-
-import controller  # noqa: F401, E402
-import main  # noqa: F401, E402
-import models  # noqa: F401, E402
-import schema  # noqa: F401, E402
+from resumeapi import controller  # noqa: F401, E402
+from resumeapi import main  # noqa: F401, E402
+from resumeapi import models  # noqa: F401, E402
