9719b58a21502c259e65d36cbf8409a1cddabf10
Eric Rochow <=>
2022-10-03 13:51:30 -0400

    Docstrings for models and env var defaults to str

--- resumeapi/models.py
+++ resumeapi/models.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Database models and pydantic schemas for use with by the API."""
+# pylint: disable=too-few-public-methods
 
 from enum import Enum
 import logging
@@ -8,12 +10,14 @@ from typing import Dict, List, Optional
 
 from pydantic import BaseModel, EmailStr
 from sqlmodel import Field, SQLModel, UniqueConstraint, create_engine
-
+from sqlalchemy.engine import Engine
 
 logger = logging.getLogger(__name__)
 
 
-class User(SQLModel, table=True):
+class User(SQLModel, table=True):  # noqa: D101
+    """User table and object model."""
+
     __table_args__ = (UniqueConstraint("username"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field()
@@ -21,10 +25,14 @@ class User(SQLModel, table=True):
     disabled: bool = Field(default=False)
 
 
-class Users(BaseModel):
+class Users(BaseModel):  # noqa: D101
+    """Users object model."""
+
     __root__: List[User]
 
-    class Config:
+    class Config:  # pylint: disable=too-few-public-methods
+        """Users configuration."""
+
         schema_extra = {
             "example": {
                 "users": [
@@ -35,11 +43,15 @@ class Users(BaseModel):
         }
 
 
-class Token(BaseModel):
+class Token(BaseModel):  # noqa: D101
+    """Token object model."""
+
     access_token: str
     token_type: str
 
-    class Config:
+    class Config:  # pylint: disable=too-few-public-methods
+        """Token configuration."""
+
         schema_extra = {
             "example": {
                 "access_token": "long_bearer_token_here",
@@ -48,13 +60,17 @@ class Token(BaseModel):
         }
 
 
-class BasicInfo(SQLModel, table=True):
+class BasicInfo(SQLModel, table=True):  # noqa: D101
+    """Basic info table and object model."""
+
     __table_args__ = (UniqueConstraint("fact"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     fact: str = Field()
     value: str
 
-    class Config:
+    class Config:  # noqa: D106
+        """BasicInfo configuration."""
+
         schema_extra = {
             "example": {
                 "fact": "name",
@@ -63,14 +79,18 @@ class BasicInfo(SQLModel, table=True):
         }
 
 
-class BasicInfos(BaseModel):
+class BasicInfos(BaseModel):  # noqa: D101
+    """Basic info object model."""
+
     name: str
     pronouns: List[str]
     email: EmailStr
     phone: str
     about: str
 
-    class Config:
+    class Config:  # pylint: disable=too-few-public-methods
+        """BasicInfos configuration."""
+
         schema_extra = {
             "example": {
                 "name": "John Jacobs",
@@ -82,14 +102,18 @@ class BasicInfos(BaseModel):
         }
 
 
-class Education(SQLModel, table=True):
+class Education(SQLModel, table=True):  # noqa: D101
+    """Education table and object model."""
+
     id: Optional[int] = Field(default=None, primary_key=True)
     institution: str
     degree: str
     graduation_date: int
     gpa: float
 
-    class Config:
+    class Config:  # noqa: D106
+        """Education configuration."""
+
         schema_extra = {
             "example": {
                 "id": 1,
@@ -101,7 +125,9 @@ class Education(SQLModel, table=True):
         }
 
 
-class Job(SQLModel, table=True):
+class Job(SQLModel, table=True):  # noqa: D101
+    """Job table."""
+
     id: Optional[int] = Field(default=None, primary_key=True)
     employer: str
     employer_summary: str
@@ -110,27 +136,25 @@ class Job(SQLModel, table=True):
     job_summary: str
     time: str
 
-    class Config:
+    class Config:  # noqa: D106
+        """Job configuration."""
+
         schema_extra = {
             "example": {
                 "id": 1,
                 "employer": "Acme, LLC",
                 "employer_summary": "Acme, LLC makes or sells something I think",
+                "location": "Easytown, USA",
                 "job_title": "Chief Scotch Officer",
                 "job_summary": "Report to my uncle the CEO and attend meetings",
-                "details": [{"id": 1, "detail": "Various duties as assigned"}],
-                "highlights": [
-                    {
-                        "id": 1,
-                        "highlight": "I once made my chair swivel around 64 times"
-                                     " without getting sick",
-                    }
-                ],
+                "time": "2015-Present",
             }
         }
 
 
-class JobResponse(BaseModel):
+class JobResponse(BaseModel):  # noqa: D101
+    """Job object model."""
+
     id: Optional[int] = Field(default=None, primary_key=True)
     employer: str
     employer_summary: str
@@ -141,7 +165,9 @@ class JobResponse(BaseModel):
     details: Optional[list]
     highlights: Optional[list]
 
-    class Config:
+    class Config:  # noqa: D106
+        """JobResponse configuration."""
+
         schema_extra = {
             "example": {
                 "id": 1,
@@ -161,19 +187,25 @@ class JobResponse(BaseModel):
         }
 
 
-class JobHighlight(SQLModel, table=True):
+class JobHighlight(SQLModel, table=True):  # noqa: D101
+    """Job highlight table and object model."""
+
     id: Optional[int] = Field(default=None, primary_key=True)
     highlight: str
     job_id: Optional[int] = Field(default=None, foreign_key="job.id")
 
 
-class JobDetail(SQLModel, table=True):
+class JobDetail(SQLModel, table=True):  # noqa: D101
+    """Job detail table and object model."""
+
     id: Optional[int] = Field(default=None, primary_key=True)
     detail: str
     job_id: Optional[int] = Field(default=None, foreign_key="job.id")
 
 
-class Certification(SQLModel, table=True):
+class Certification(SQLModel, table=True):  # noqa: D101
+    """Certification table and object model."""
+
     __table_args__ = (UniqueConstraint("cert"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     cert: str = Field()
@@ -182,7 +214,9 @@ class Certification(SQLModel, table=True):
     valid: bool
     progress: int
 
-    class Config:
+    class Config:  # noqa: D106
+        """Certification configuration."""
+
         schema_extra = {
             "example": {
                 "cert": "CCIE",
@@ -194,35 +228,47 @@ class Certification(SQLModel, table=True):
         }
 
 
-class Competency(SQLModel, table=True):
+class Competency(SQLModel, table=True):  # noqa: D101
+    """Competency table and object model."""
+
     __table_args__ = (UniqueConstraint("competency"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     competency: str = Field()
 
 
-class InterestType(SQLModel, table=True):
+class InterestType(SQLModel, table=True):  # noqa: D101
+    """InterestType table and object model."""
+
     __table_args__ = (UniqueConstraint("interest_type"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     interest_type: str = Field(index=True)
 
 
-class InterestTypes(str, Enum):
-    personal = "personal"
-    technical = "technical"
+class InterestTypes(str, Enum):  # noqa: D101
+    """Enum of interest types."""
+
+    personal = "personal"  # pylint: disable=invalid-name
+    technical = "technical"  # pylint: disable=invalid-name
 
 
-class Interest(SQLModel, table=True):
+class Interest(SQLModel, table=True):  # noqa: D101
+    """Interest table."""
+
     __table_args__ = (UniqueConstraint("interest"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     interest_type_id: Optional[int] = Field(default=None, foreign_key="interesttype.id")
     interest: str = Field()
 
 
-class InterestsResponse(BaseModel):
+class InterestsResponse(BaseModel):  # noqa: D101
+    """Interest object model."""
+
     personal: Optional[List[str]]
     technical: Optional[List[str]]
 
-    class Config:
+    class Config:  # noqa: D106
+        """InsterestsResponse configuration."""
+
         schema_extra = {
             "example": {
                 "personal": ["Movies", "Sports", "Books"],
@@ -231,14 +277,18 @@ class InterestsResponse(BaseModel):
         }
 
 
-class Preference(SQLModel, table=True):
+class Preference(SQLModel, table=True):  # noqa: D101
+    """Preference table and object model."""
+
     __table_args__ = (UniqueConstraint("preference"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     preference: str = Field()
     value: str
 
 
-class Preferences(BaseModel):
+class Preferences(BaseModel):  # noqa: D101
+    """Preference object model."""
+
     OS: List[str]
     EDITOR: str
     TERMINAL: str
@@ -248,7 +298,9 @@ class Preferences(BaseModel):
     LANGUAGES: List[str]
     TEST_SUITES: List[str]
 
-    class Config:
+    class Config:  # noqa: D106
+        """Preferences configuration."""
+
         schema_extra = {
             "example": {
                 "OS": ["Favorite OS 1", "Favorite OS 2"],
@@ -263,14 +315,18 @@ class Preferences(BaseModel):
         }
 
 
-class SideProject(SQLModel, table=True):
+class SideProject(SQLModel, table=True):  # noqa: D101
+    """Side project table and object model."""
+
     __table_args__ = (UniqueConstraint("title"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field()
     tagline: str
     link: str
 
-    class Config:
+    class Config:  # noqa: D106  # pylint: disable=too-few-public-methods
+        """SideProject configuration."""
+
         schema_extra = {
             "example": {
                 "title": "my_project",
@@ -280,40 +336,52 @@ class SideProject(SQLModel, table=True):
         }
 
 
-class SocialLinkEnum(str, Enum):
-    LinkedIn = "linkedin"
-    Github = "github"
-    Twitter = "twitter"
-    Matrix_im = "matrix_im"
-    Website = "website"
-    Resume = "resume"
-    Facebook = "facebook"
+class SocialLinkEnum(str, Enum):  # noqa: D101
+    """Enum of available social links."""
+
+    LinkedIn = "linkedin"  # pylint: disable=invalid-name
+    Github = "github"  # pylint: disable=invalid-name
+    Twitter = "twitter"  # pylint: disable=invalid-name
+    Matrix_im = "matrix_im"  # pylint: disable=invalid-name
+    Website = "website"  # pylint: disable=invalid-name
+    Resume = "resume"  # pylint: disable=invalid-name
+    Facebook = "facebook"  # pylint: disable=invalid-name
 
 
-class SocialLink(SQLModel, table=True):
+class SocialLink(SQLModel, table=True):  # noqa: D101
+    """Social link table and object model."""
+
     __table_args__ = (UniqueConstraint("platform"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     platform: str = Field()
     link: str
 
-    class Config:
+    class Config:  # noqa: D106
+        """SocialLink configuration."""
+
         schema_extra = {
             "platform": "linkedin",
             "link": "https://linkedin.com/in/my_user",
         }
 
 
-class Skill(SQLModel, table=True):
+class Skill(SQLModel, table=True):  # noqa: D101
+    """Skill table and object model."""
+
     __table_args__ = (UniqueConstraint("skill"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     skill: str = Field()
     level: int
 
-    class Config:
+    class Config:  # noqa: D106
+        """Skill configuration."""
+
         schema_extra = {"example": {"skill": "Git", "level": 75}}
 
 
-class FullResume(BaseModel):
+class FullResume(BaseModel):  # noqa: D101
+    """Full resume object model with all configured sections."""
+
     basic_info: BasicInfos
     certifications: List[Certification]
     competencies: List[str]
@@ -326,7 +394,15 @@ class FullResume(BaseModel):
     social_links: List[SocialLink]
 
 
-def configure_engine(engine_echo: bool = False):
+def configure_engine(engine_echo: bool = False) -> Engine:
+    """
+    Generate the SQLAlchemy engine for use by the API.
+
+    :param engine_echo: Whether to have the SQL output echo'd by the engine
+    :type engine_echo: bool, optional
+    :return: Engine used by SQLModel
+    :rtype: sqlalchemy.engine.Engine
+    """
     db_type = os.getenv("DB_TYPE", default="sqlite")
     db_host = os.getenv("DB_HOST", default="resumedb")
     db_name = os.getenv("DB_NAME", default="resume")
@@ -343,7 +419,7 @@ def configure_engine(engine_echo: bool = False):
         sql_engine = create_engine(f"sqlite:///{sqlite_file}", echo=engine_echo)
     elif db_type.lower() == "postgresql":
         logger.debug("postgresql configuration db type detected")
-        db_port = os.getenv("DB_PORT", default=5432)
+        db_port = os.getenv("DB_PORT", default="5432")
         sql_engine = create_engine(
             f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}",
             echo=engine_echo,
