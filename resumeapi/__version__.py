39c9f1add0cb9cc995c6dc0461e4aae43a064881
Eric Rochow <=>
2022-10-03 13:52:15 -0400

    init __version__.py

---
+++ resumeapi/__version__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+__author__ = "Eric Rochow"
+__author_email__ = "ericrochow@gmail.com"
+__description__ = "API that provides my resume"
+__maintainer__ = "Eric Rochow"
+__maintainer_email__ = "ericrochow@gmail.com"
+__version__ = "0.2.0"
